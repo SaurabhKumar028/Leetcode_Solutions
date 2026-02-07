09ded7db2ab687ab4b42b6e0248472b3464e1666
Saurabh Kumar <saurabh.shukla1778@gmail.com>
2026-02-07 14:22:27 +0530

    Time: 139 ms (5.26%), Space: 48 MB (16.28%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -5,13 +5,25 @@ class Solution {
         char [] arr = s.toCharArray();
         int count = 0 ;
         
-        for(int i = 0 ; i< arr.length ; i++){
-            if(!st.isEmpty() && st.peek() == 'b' && arr[i] == 'a'){
+        // for(int i = 0 ; i< arr.length ; i++){
+        //     if(!st.isEmpty() && st.peek() == 'b' && arr[i] == 'a'){
+        //         st.pop();
+        //         count++;
+        //     }
+        //     else{
+        //         st.push(arr[i]);
+        //     }
+        // }
+
+        // ALTERNATE APPROACH  BY FOR EACH LOOP  -- >
+
+         for(char Ch : s.toCharArray()){
+            if(!st.isEmpty() && st.peek() == 'b' && Ch == 'a'){
                 st.pop();
                 count++;
             }
             else{
-                st.push(arr[i]);
+                st.push(Ch);
             }
         }
         return count;
